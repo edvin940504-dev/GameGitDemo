9ad547e639d291ac2a620cdcf3bd025cd3954cfc
Edvin <ediparmeza94@yahoo.com>
2022-11-12 00:32:56 +0100

    Changed the hello world message

--- GameGitDemo/Program.cs
+++ GameGitDemo/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace GameGitDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Version Control :)");
+            Console.ReadKey(true);
+        }
+    }
+}
