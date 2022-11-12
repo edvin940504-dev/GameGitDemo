c8e5428e0cd925f32dcd69a12291a10c1c1811d4
Edvin <ediparmeza94@yahoo.com>
2022-11-12 21:12:29 +0100

    Added Player initialization to the game

--- GameGitDemo/Game.cs
+++ GameGitDemo/Game.cs
@@ -13,6 +13,11 @@ namespace GameGitDemo
         {
             WriteLine("=== Welcome to the game ===");
 
+            Write("What is your name?");
+            string name = ReadLine();
+            Player currentPlayer = new Player(name);
+            WriteLine($"Let's get started {currentPlayer.Name}!");
+
             WriteLine("\nPress any key to exit...");
             ReadKey(true);
         }
