44e5ce7747f5885578cfaf1895c710490844fc29
Edvin <ediparmeza94@yahoo.com>
2022-11-12 20:37:26 +0100

    Added a new message

--- GameGitDemo/Program.cs
+++ GameGitDemo/Program.cs
@@ -7,6 +7,7 @@ namespace GameGitDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Version Control :)");
+            Console.WriteLine("My name is Edvin");
             Console.ReadKey(true);
         }
     }
